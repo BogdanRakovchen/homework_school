231a9b138d72a6103c2d5523eb870c9694dbe7f4
Bogdan Rakovchen <bogdan-dvzd.rzd@mail.ru>
2022-09-08 12:00:06 +1000

    Initial commit

---
+++ src/main/java/com/homework/homework_school/HomeworkSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.homework.homework_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkSchoolApplication.class, args);
+    }
+
+}
