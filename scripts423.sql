97e5950b55a9b67bd63a85bd61dd7561daa01e50
Bogdan Rakovchen <bogdan-dvzd.rzd@mail.ru>
2022-10-20 02:49:02 +1000

    поработал с sql добавив три файла scripts

--- scripts423.sql
+++ scripts423.sql
@@ -2,4 +2,4 @@ SELECT student.name, student.age, faculty.name FROM student
 INNER JOIN faculty ON student.faculty_id = faculty.id;
 
 SELECT student.name FROM student
-INNER JOIN avatar ON student.id = avatar.student_id;
+LEFT JOIN avatar ON student.id = avatar.student_id;
