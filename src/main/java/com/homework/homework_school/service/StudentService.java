3defd766f66f91f163cf821faade41fb390675ba
Bogdan Rakovchen <bogdan-dvzd.rzd@mail.ru>
2022-10-25 03:01:35 +1000

    логирование

--- src/main/java/com/homework/homework_school/service/StudentService.java
+++ src/main/java/com/homework/homework_school/service/StudentService.java
@@ -5,9 +5,11 @@ import com.homework.homework_school.model.Student;
 import com.homework.homework_school.repository.FacultyRepository;
 import com.homework.homework_school.repository.StudentRepository;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.slf4j.LoggerFactory;
 
 
 import java.util.Collection;
@@ -20,27 +22,34 @@ public class StudentService {
 
     private final StudentRepository studentRepository;
 
+    public Logger logger = LoggerFactory.getLogger(StudentService.class);
+
     public StudentService(StudentRepository studentRepository) {
 
         this.studentRepository = studentRepository;
     }
 
     public Student createStudent(Student student) {
-
+        logger.info("Was invoked method for create student");
         return studentRepository.save(student);
     }
 
     public Student getStudent(Long id) {
-        return studentRepository.findById(id).get();
+            logger.error("getAvatar равен null");
+            logger.info("getStudent show");
+        return studentRepository.findById(id).orElse(null).getAvatar().getStudent();
+
 
     }
 
     public Faculty getFacultyOfStudent(Long id) {
+        logger.error("There is not student with id = " + id);
         return studentRepository.findById(id).orElse(null).getFaculty();
 
     }
     
     public Student getAllStudent(int age) {
+        logger.info("getAllStudent for age");
         List<Student> student = studentRepository.findAll();
         for (Student student1 : student) {
             if(student1.getAge() == age) {
@@ -51,27 +60,34 @@ public class StudentService {
     }
 
     public Collection<Student> findByAgeBetween(int minAge, int maxAge) {
-       return studentRepository.findByAgeBetween(minAge, maxAge);
+        logger.info("Collection getAllStudent for minAge and maxAge");
+        return studentRepository.findByAgeBetween(minAge, maxAge);
     }
 
     public Student updateStudent(Long id, Student student) {
+        logger.info("updateStudent " + student);
+
         return studentRepository.save(student);
     }
 
     public void deleteStudent(Long id) {
+        logger.info("updateStudent, id = " + id);
         studentRepository.deleteById(id);
 
     }
 
     public int getAllStudents() {
+        logger.info("allstudents");
         return studentRepository.findByAllStudent();
     }
 
     public double getAvarageAgeAllStudents() {
+        logger.info("getAvarageAgeAllStudents");
         return studentRepository.findByAvarageAgeAllStudents();
     }
 
     public Collection<Student> getFiveLastOfStudents() {
+        logger.info("getFiveLastOfStudents");
         return studentRepository.findByFiveLastOfStudents();
     }
 
--- src/main/resources/liquibase/scripts/students.sql
+++ src/main/resources/liquibase/scripts/students.sql
@@ -1,10 +1,10 @@
 --liquibase formatted sql
 
---changeset brakovchen:1
-CREATE INDEX search_student ON student(name);
-
---changeset brakovchen:2
-CREATE INDEX search_faculty_of_color_or_name ON faculty(color, name);
+-- --changeSet brakovchen:1
+-- CREATE INDEX search_student ON student(name);
+--
+-- --changeSet brakovchen:2
+-- CREATE INDEX search_faculty_of_color_or_name ON faculty(color, name);
 
 --changeset brakovchen:3
 CREATE TABLE u(
